f95b39f8c02d035404d53dd97f0a9aad6cf77683
HermelaCodes <hermellaseltanu@gmail.com>
2024-05-19 02:17:08 +0300

    Create variables.py

---
+++ day_2/variables.py
@@ -0,0 +1,13 @@
+# Day 2: 30 Days of python programming
+first_name=Hermela
+last_name=Seltanu
+fill_name=Hermela Seltanu
+country=Kenya
+city=Nairobi
+age=22
+year=2024
+is_married=False
+is_true=Yes
+is_light_on=Yes
+age, year, first_name='22','2024','Hermela'
+print(first_name)
